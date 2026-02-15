ec14cb0f78a62abc5f58ce22a569487bebdb5fba
erikaseascope <erikaseascope@proton.me>
2026-02-15 17:01:17 -0500

    Update __init__.py
    Add ui/__init__.py - UI package init

--- src/neuroforge/ui/__init__.py
+++ src/neuroforge/ui/__init__.py
@@ -1,1 +1,9 @@
+"""
+NeuroForge UI Package
+Streamlit dashboard + HITL gates
+"""
 
+# If dashboard.py gets a main() function later, export it here
+# from .dashboard import run_dashboard
+
+__all__ = []  # expand later
